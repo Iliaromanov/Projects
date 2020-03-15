466180670a7c482d592973ed1083984ce98a2e56
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-03-15 14:47:11 -0400

    Update person.py

--- economy_game/person.py
+++ economy_game/person.py
@@ -12,3 +12,17 @@ class Person:
     def earn_money(self, amount: float) -> None:
         self.net_worth += amount
         Person.all_ppl[self.name] = f"{self.economic_class}, Net Worth: ${self.net_worth}"
+
+    def calc_economic_class(self) -> str:
+        if self.net_worth <= 70000:
+            self.economic_class = "lower class"
+        elif self.net_worth <= 300000:
+            self.economic_class = "middle class"
+        elif self.net_worth >= 300000 and self.net_worth <= 2000000:
+            self.economic_class = "upper middle class"
+        elif self.net_worth >= 2000000:
+            self.economic_class = "upper class"
+
+        Person.all_ppl[self.name] = f"{self.economic_class}, Net Worth: ${self.net_worth}"
+
+        return self.economic_class
