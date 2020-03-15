781d1162fee6f78cf4a34e9dc993ece4db132e26
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-03-15 14:43:50 -0400

    Update person.py

--- economy_game/person.py
+++ economy_game/person.py
@@ -1,1 +1,10 @@
+from typing import Dict
 
+class Person:
+    all_ppl = {}
+
+    def __init__(self, name: str):
+        self.name = name
+        self.economic_class = "lower class"
+        self.net_worth = 0
+        Person.all_ppl[self.name] = f"{self.economic_class}, Net Worth: ${self.net_worth}"
