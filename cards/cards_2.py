7a39dd40775b4b740c0ad3380907e9798f35de44
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-03-25 11:23:24 -0400

    Update cards_2.py

--- cards/cards_2.py
+++ cards/cards_2.py
@@ -1,10 +1,10 @@
 # A class for creating and organizing playing cards
 class PlayingCard:
     full_deck = []
-    hearts = []
-    diamonds = []
-    spades = []
-    clubs = []
+    hearts = {}
+    diamonds = {}
+    spades = {}
+    clubs = {}
 
     def __init__(self, card_value: int, suite: str):
         self.card_value = card_value
@@ -14,6 +14,39 @@ class PlayingCard:
     def __str__(self) -> str:
         return f"{self.card_value}, {self.suite}"
     
+    @staticmethod
+    def make_cards():
+        names = ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king']
+
+        i = 0
+
+        while i < 13:
+            PlayingCard.hearts[names[i]] = PlayingCard(i+1, 'hearts')
+            
+            i += 1
+
+        j = 0
+
+        while j < 13:
+            PlayingCard.diamonds[names[j]] = PlayingCard(j+1, 'diamonds')
+            
+            j += 1
+
+        k = 0
+
+        while k < 13:
+            PlayingCard.spades[names[k]] = PlayingCard(k+1, 'spades')
+            
+            k += 1
+
+        l = 0
+
+        while l < 13:
+            PlayingCard.clubs[names[l]] = PlayingCard(l+1, 'clubs')
+            
+            l += 1
+
+'''
     @staticmethod
     def sort():
         """Organizes cards into suites and assigns them color
@@ -31,3 +64,8 @@ class PlayingCard:
             elif card.suite == 'clubs':
                 PlayingCard.clubs.append(card)
                 card.color = 'black'
+'''
+
+PlayingCard.make_cards()
+
+print(len(PlayingCard.full_deck))
